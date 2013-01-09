985c964ed01a417cae7017d01b41991d4f2bd42b
Emerson <emerson.broga@gmail.com>
2013-01-09 00:34:57 -0200

    Plugin using an OO aproach.

--- src/mailrelay.php
+++ src/mailrelay.php
@@ -1,5 +1,4 @@
 <?php 
-
 /*
 Plugin Name: Mailrelay
 Plugin URI: http://mailrelay.com
@@ -7,80 +6,142 @@ Description: Easily sync your Wordpress users with Mailrelay.
 Author: Mailrelay.com
 Version: 1.1.1
 */
+add_action('admin_menu', array('MailRelay', 'addMenuPage'));
 
-//*************** Admin function ***************
-function step1() 
+
+Class MailRelay
 {
-	include('step1.php');
-}
+    
+    const slug = 'mailrelay';
+    const url  = 'http://%s/ccm/admin/api/version/2/&type=%s';
+    
+    private $step, $username, $password, $userhost, $apikey, $groups;
 
-function step2() 
-{
-	include('step2.php');
-}
+    public $message = null;
+    
+    public function getMessage()
+    {
+        return $this->message;
+    }
+    
+    public function setMessage( $value )
+    {
+        return $this->message = $value;
+    }
+    
+    public function getServiceUrl( $format = 'json')
+    {
+        return sprintf(self::url, $this->userhost, $format);
+    }
+    
+    public function addMenuPage()
+    {
+        // Add the top-level admin menu
+        $page_title = 'Mail Relay';
+        $menu_title = 'Mail Relay';
+        $capability = 'manage_options';
+        $menu_slug  = self::slug;
+        $function   = array(new MailRelay(), 'controller');
+        $icon_url   = null;
+        $position   = null;
 
-function step3() 
-{
-	include('step3.php');
-}
+        
+        add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
+        
+    }
+    
+    public function controller()
+    {
+        $this->step = ( !isset($_REQUEST['step']) ) ? 'step1' : $_REQUEST['step'];
+        switch($this->step)
+        {
+            case 'step2':
+                $this->step2();
+                break;
+            case 'step3':
+                $this->step3();
+                break;
+            default:
+                $this->step1();
+                break;
+        }
+       
+    }
+    
+    public function step1()
+    {
+       include('step1.php');
+    } 
+    
+    public function step2()
+    {
+        //Get the Values
+        $this->username = (isset($_POST['usrname']))  ? $_POST['usrname']  : get_option('usrname');
+        $this->password = (isset($_POST['pwd']))      ? $_POST['pwd']      : get_option('pwd');
+        $this->userhost = (isset($_POST['userhost'])) ? $_POST['userhost'] : get_option('userhost');
+        
+        //Update the Values
+        update_option('usrname', $this->username);
+        update_option('pwd', $this->password);
+        update_option('userhost', $this->userhost);
 
-function web_admin_actions() 
-{
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
-}
-
-function web_admin_actions1() 
-{
-	$_REQUEST["step"] = "step1";
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
-}
-
-function web_admin_actions2() 
-{
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step2");
-}
-
-function web_admin_actions3() 
-{
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step3");
-}
-
-function mailrelay_init()
-{
-	$result = load_plugin_textdomain('mailrelay', false, dirname(plugin_basename(__FILE__)).'/languages');
-}
-
-if(!isset($_REQUEST['step']) || ($_REQUEST['step']=='step1') )
-{
-	add_action('admin_menu', 'web_admin_actions');
-}
-add_action('init', 'mailrelay_init');
-
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step2') )
-{
-	//Form data sent
-	$usrname = $_POST['usrname'];
-	update_option('usrname', $usrname);
-
-	$pwd = $_POST['pwd'];
-	update_option('pwd', $pwd);
-
-	$userhost = $_POST['userhost'];
-	update_option('userhost', $userhost);
-
-	// These will be entered by user. 
-	$username = get_option('usrname');
-	$password = get_option('pwd');
-	$hostname = get_option('userhost');
-
+        
 	// First thing, authenticate
-	$url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
+	$jsonResult = $this->authenticate();
+
+        if(!$jsonResult){
+            $this->setMessage('Invalid host, username or password. Please Retry.');   
+            $this->step1();
+            return;
+        }
+        
+        if($jsonResult->error == 'Your account does not have an API key.'){
+            $this->setMessage ('Your account does not have an API key. Please, generate one in your Mailrelay\'s account: Settings -> API access -> Generate new API key.');
+            $this->step1();
+            return;
+        }
+        
+        // error with API
+        if(!$jsonResult->data){
+            
+            $this->setMessage ('Invalid host, username or password. Please Retry.');
+            $this->step1();
+            return;
+        }
+        
+        //Set Api Key
+        $this->apikey = $jsonResult->data;
+        
+        //Get Groups
+        $this->groups = $this->getGroups();
+        
+        if(!$this->groups){
+            $this->setMessage ('Gruoups not found. Please Retry.');
+            $this->step1();
+            return;
+        }
+            
+        include('step2.php');
+    } 
+    
+    public function step3()
+    {
+    
+        //@TODO: implement Step 3
+    }
+    
+    
+    public function authenticate()
+    {
+        
+        // First thing, authenticate
+	$url  = $this->getServiceUrl();
 	$curl = curl_init($url);
 
 	$params = array(
 		'function' => 'doAuthentication',
-		'username' => $username,
-		'password' => $password
+		'username' => $this->username,
+		'password' => $this->password
 	);
 
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -95,205 +156,45 @@ if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step2') )
 	// Call the page, it will return a json
 	$result = curl_exec($curl);
 	$jsonResult = json_decode($result);
-
-	if (!$jsonResult || trim($jsonResult->status)!=1)
-	{
-		global $message;
-		add_action('admin_menu', 'web_admin_actions1');
-
-		if ($jsonResult->error == 'Your account does not have an API key.') {
-			$message = "Your account does not have an API key. Please, generate one in your Mailrelay's account: Settings -> API access -> Generate new API key.";
-		} else {
-			$message = 'Invalid host, username or password. Please Retry.';
-		}
-	}
-	else
-	{
-		$apiKey = $jsonResult->data;
-		if ($apiKey)
-		{
-			// Call getGroups
-			$params = array(
-				'function' => 'getGroups',
-				'apiKey' => $apiKey,
-				'sortField' => 'name',
-				'sortOrder' => 'ASC'
-			);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-       			$headers = array(
-                		'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        		);
-        		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-			$result = curl_exec($curl);
-			$jsonResult = json_decode($result);
-			update_option('jsonResult', $jsonResult);
-			add_action('admin_menu', 'web_admin_actions2');
-		}
-		else
-		{
-			// error with API
-			add_action('admin_menu', 'web_admin_actions1');
-			$message = 'Invalid host, username or password. Please Retry.';
-		}
-	}
-} 
-else 
-{
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
-}
-
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step3') )
-{
-	$querystr = "SELECT * FROM $wpdb->users";
-	$users = $wpdb->get_results($querystr, OBJECT);
-	$groups=$_POST['group'];
-
-	//Form data sent
-	$usrname = $_POST['usrname'];
-	update_option('usrname', $usrname);
-	
-	$pwd = $_POST['pwd'];
-	update_option('pwd', $pwd);
-	
-	$userhost = $_POST['userhost'];
-	update_option('userhost', $userhost);
-	update_option('step', 'step2');
-
-	// These will be entered by user.
-	$username = get_option('usrname');
-	$password = get_option('pwd');
-	$hostname = get_option('userhost');
-
-	// First thing, authenticate
-	$url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
-	$curl = curl_init($url);
-
+       
+        if( !$jsonResult || trim($jsonResult->status)!=1 )
+            return false;
+        else
+            return $jsonResult;
+    }
+    
+    public function getGroups()
+    {
+        
+        // Call getGroups
 	$params = array(
-		'function' => 'doAuthentication',
-		'username' => $username,
-		'password' => $password
-	);
-
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_POST, 1);
+            'function'  => 'getGroups',
+            'apiKey'    => $this->apikey,
+            'sortField' => 'name',
+            'sortOrder' => 'ASC'
+        );
+        
+        $url = $this->getServiceUrl();
+        $curl = curl_init( $url );
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
-        $headers = array(
-                'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        );
+       	$headers = array(
+                    'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
+                   );
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-	// Call the page, it will return a json
-	$result = curl_exec($curl);
-	$jsonResult = json_decode($result);
-
-	if(trim($jsonResult->status)==1)
-	{
-		$added=0;
-		$updated=0;
-		$fail=0;
-
-		if (!$jsonResult->status) 
-		{
-			throw new Exception('Authenticate failed. Please verify hostname, username and password.');
-		}
-		else
-		{
-			$apiKey = $jsonResult->data;
-		}
-
-		foreach($users as $user)
-		{
-			$user->user_email;
-			// Call getSubscribers
-			$params = array(
-				'function' => 'getSubscribers',
-				'apiKey' => $apiKey,
-				'email'=>$user->user_email,
-			);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-        		$headers = array(
-                		'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        		);
-        		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-			$result = curl_exec($curl);
-			$jsonResult = json_decode($result);
-
-			if(count($jsonResult->data)>0)
-			{
-				$params = array(
-					'function' => 'updateSubscriber',
-					'apiKey' => $apiKey,
-					'id'=>$jsonResult->data[0]->id,
-					'email'=>$user->user_email,
-					'name'=>$user->display_name,
-					'groups'=>$groups
-				);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-        			$headers = array(
-                			'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        			);
-        			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-				$result = curl_exec($curl);
-				$jsonResult = json_decode($result);
-
-				if($jsonResult->status==1)
-				{
-					$updated++;
-				}
-				else
-				{
-					$fail++;
-				}
-			}
-			else
-			{
-				$params = array(
-					'function' => 'addSubscriber',
-					'apiKey' => $apiKey,
-					'email'=>$user->user_email,
-					'name'=>$user->display_name,
-					'groups'=>$groups
-				);
-
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-        			$headers = array(
-                			'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        			);
-        			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-				$result = curl_exec($curl);
-				$jsonResult = json_decode($result);
-
-				if($jsonResult->status==1)
-				{
-					$added++;
-				}
-				else
-				{
-					$fail++;
-				}
-			}
-		}
-		update_option('added', $added);
-		update_option('updated', $updated);
-		update_option('fail', $fail);
-	}
-	add_action('admin_menu', 'web_admin_actions3');
-} 
-else 
-{
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        
+        $result = curl_exec($curl);
+        $jsonResult = json_decode($result);
+        
+        if( $jsonResult && $jsonResult->status == 1 && isset($jsonResult->data)){
+            update_option('jsonResult', $jsonResult);
+            return $jsonResult->data;
+        }else{
+            return false;
+        }
+        
+    }
 }
+
 ?>
--- src/step2.php
+++ src/step2.php
@@ -1,67 +1,54 @@
-<?php 
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step2') )
-{
-	$jsonResult = get_option('jsonResult');
-	$data=$jsonResult->data;
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
-	
-?>
-
 <div class="wrap">
-<?php
-        screen_icon('options-general'); 
-        echo "<h2 id='mailrelay_settings'>";
-        echo _e( 'Mairelay Step 2 - Choose groups', 'mailrelay' ) . "</h2>"; 
-?>
+    <?php screen_icon('options-general');  ?>
+    <h2 id='mailrelay_settings'><?php _e( 'Mairelay Step 2 - Choose groups', $this->slug ); ?></h2> 
 
-<form name="webservices_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+
+    <form name="webservices_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
 	<input type="hidden" name="chk_hidden" value="Y" />
 	<input type="hidden" name="step" value="step3" />
-	<input type="hidden" name="usrname" value="<?php echo $usrname; ?>">
-	<input type="hidden" name="pwd" value="<?php echo $pwd; ?>" />
-	<input type="hidden" name="userhost" value="<?php echo $userhost; ?>">
+	<input type="hidden" name="usrname" value="<?php echo $this->username; ?>">
+	<input type="hidden" name="pwd" value="<?php echo $this->password; ?>" />
+	<input type="hidden" name="userhost" value="<?php echo $this->userhost; ?>">
 
 	<table class="form-table">
-		<tr><th scope="row">
-		<label for="group"><?php _e( 'Please Select Group', 'mailrelay' ); ?></label></th>
+            <tr>
+                <th scope="row">
+                    <label for="group"><?php _e( 'Please Select Group', 'mailrelay' ); ?></label>
+                </th>
 		<td>
-		<select multiple="multiple" name="group[]" size="5" style="height:auto;">
-			<?php foreach($data as $x=>$value){ ?>
-			<option value="<?php echo $data[$x]->id; ?>"><?php echo $data[$x]->name; ?></option>
-			<?php } ?>
-		</select>
-		<p><?php _e("All your Wordpress users will be synced with the groups you are choosing now.", "mailrelay"); ?><br />
-		<?php _e("To create new groups in Mailrelay, you must login into the control panel and click into the Mail Relay > Subscribers groups", "mailrelay"); ?><br />
-		<?php _e("Once there you can add a new group for your Wordpress users, or edit an existing one", "mailrelay"); ?></p>
-		</td></tr>
+                    <select multiple="multiple" name="group[]" size="5" style="height:auto;">
+			<?php foreach($this->groups as $key => $group ): ?>
+                            <option value="<?php echo $group->id; ?>"><?php echo $group->name; ?></option>
+			<?php endforeach; ?>
+                    </select>
+                    <p>
+                        <?php _e('All your Wordpress users will be synced with the groups you are choosing now.', $this->slug); ?><br />
+                        <?php _e('To create new groups in Mailrelay, you must login into the control panel and click into the Mail Relay > Subscribers groups', $this->slug); ?><br />
+                        <?php _e('Once there you can add a new group for your Wordpress users, or edit an existing one', $this->slug); ?>
+                    </p>
+		</td>
+            </tr>
 	</table>
 
-	<p class="submit">
-	<input type="button" onclick="return chk_form();" name="Select groups" value="<?php _e('Select groups', 'mailrelay' ) ?>" class="button-primary" />
+        <p class="submit">
+            <input type="button" onclick="return check_form();" name="Select groups" value="<?php _e('Select groups', $this->slug ) ?>" class="button-primary" />
 	</p>
 </form>
 
 </div>
 <script type="text/javascript">
-function chk_form()
-{
-	var chk=check();
-	if(chk!=false)
-	{
-		document.webservices_form.submit();
-		document.webservices_form.action="";
-	}
-}
-     function check() {
-		if(document.webservices_form["group[]"].value == "")
-		{
-			alert("<?php echo _e('Please select at least one Group.', 'mailrelay'); ?>");
-			return false; 
-		}
-		return true;
-	}
-  </script>
-
-<?php } ?>
+    function check_form()
+    {
+        if( document.webservices_form["group[]"].value === "" ){
+            
+            alert("<?php echo _e('Please select at least one Group.', $this->slug ); ?>");
+            return false; 
+            
+        }else{
+            
+            document.webservices_form.submit();
+            document.webservices_form.action="";
+            
+        }
+    }
+</script>
