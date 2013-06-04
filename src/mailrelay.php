78fa2663726e565a12cb6825342f44832ffb6e3a
Julio Cesar Laura <contact@julio-laura.com>
2013-06-03 17:26:25 -0700

    MRPLUGINS-1 Change plugins to use api key SOLVED
    Now the plugin requires only the host and the api key to work

--- src/mailrelay.php
+++ src/mailrelay.php
@@ -1,299 +1,234 @@
-<?php 
+<?php
 
 /*
 Plugin Name: Mailrelay
 Plugin URI: http://mailrelay.com
 Description: Easily sync your Wordpress users with Mailrelay.
 Author: Mailrelay.com
-Version: 1.1.1
+Version: 1.2.0
 */
 
 //*************** Admin function ***************
-function step1() 
+function step1()
 {
-	include('step1.php');
+    include('step1.php');
 }
 
-function step2() 
+function step2()
 {
-	include('step2.php');
+    include('step2.php');
 }
 
-function step3() 
+function step3()
 {
-	include('step3.php');
+    include('step3.php');
 }
 
-function web_admin_actions() 
+function web_admin_actions()
 {
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
+    add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
 }
 
-function web_admin_actions1() 
+function web_admin_actions1()
 {
-	$_REQUEST["step"] = "step1";
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
+    $_REQUEST["step"] = "step1";
+    add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step1");
 }
 
-function web_admin_actions2() 
+function web_admin_actions2()
 {
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step2");
+    add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step2");
 }
 
-function web_admin_actions3() 
+function web_admin_actions3()
 {
-	add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step3");
+    add_options_page("Mailrelay", "Mailrelay", 1, "Mailrelay", "step3");
 }
 
 function mailrelay_init()
 {
-	$result = load_plugin_textdomain('mailrelay', false, dirname(plugin_basename(__FILE__)).'/languages');
+    $result = load_plugin_textdomain('mailrelay', false, dirname(plugin_basename(__FILE__)).'/languages');
 }
 
-if(!isset($_REQUEST['step']) || ($_REQUEST['step']=='step1') )
+if(!isset($_REQUEST['step']) || ($_REQUEST['step'] == 'step1'))
 {
-	add_action('admin_menu', 'web_admin_actions');
+    add_action('admin_menu', 'web_admin_actions');
 }
 add_action('init', 'mailrelay_init');
 
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step2') )
+if(isset($_REQUEST['step']) && ($_REQUEST['step'] == 'step2'))
 {
-	//Form data sent
-	$usrname = $_POST['usrname'];
-	update_option('usrname', $usrname);
+    //Form data sent
+    $userhost = $_POST['userhost'];
+    update_option('userhost', trim($userhost));
 
-	$pwd = $_POST['pwd'];
-	update_option('pwd', $pwd);
+    $mailrelay_api_key = $_POST['mailrelay_api_key'];
+    update_option('mailrelay_api_key', trim($mailrelay_api_key));
 
-	$userhost = $_POST['userhost'];
-	update_option('userhost', $userhost);
+    // These will be entered by user.
+    $hostname = get_option('userhost');
+    $mailrelay_api_key = get_option('mailrelay_api_key');
 
-	// These will be entered by user. 
-	$username = get_option('usrname');
-	$password = get_option('pwd');
-	$hostname = get_option('userhost');
+    // First thing, authenticate
+    $url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
+    $curl = curl_init($url);
 
-	// First thing, authenticate
-	$url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
-	$curl = curl_init($url);
+    // Call getGroups
+    $params = array(
+        'function' => 'getGroups',
+        'apiKey' => $mailrelay_api_key,
+        'sortField' => 'name',
+        'sortOrder' => 'ASC'
+    );
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
-	$params = array(
-		'function' => 'doAuthentication',
-		'username' => $username,
-		'password' => $password
-	);
+    $headers = array(
+        'X-Request-Origin: Wordpress|1.2.0|'.$wp_version
+    );
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_POST, 1);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+    $result = curl_exec($curl);
+    $jsonResult = json_decode($result);
 
-        $headers = array(
-        	'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        );
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    if (!$jsonResult || trim($jsonResult->status) != 1)
+    {
+        global $message;
+        add_action('admin_menu', 'web_admin_actions1');
 
-	// Call the page, it will return a json
-	$result = curl_exec($curl);
-	$jsonResult = json_decode($result);
-
-	if (!$jsonResult || trim($jsonResult->status)!=1)
-	{
-		global $message;
-		add_action('admin_menu', 'web_admin_actions1');
-
-		if ($jsonResult->error == 'Your account does not have an API key.') {
-			$message = "Your account does not have an API key. Please, generate one in your Mailrelay's account: Settings -> API access -> Generate new API key.";
-		} else {
-			$message = 'Invalid host, username or password. Please Retry.';
-		}
-	}
-	else
-	{
-		$apiKey = $jsonResult->data;
-		if ($apiKey)
-		{
-			// Call getGroups
-			$params = array(
-				'function' => 'getGroups',
-				'apiKey' => $apiKey,
-				'sortField' => 'name',
-				'sortOrder' => 'ASC'
-			);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-       			$headers = array(
-                		'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        		);
-        		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-			$result = curl_exec($curl);
-			$jsonResult = json_decode($result);
-			update_option('jsonResult', $jsonResult);
-			add_action('admin_menu', 'web_admin_actions2');
-		}
-		else
-		{
-			// error with API
-			add_action('admin_menu', 'web_admin_actions1');
-			$message = 'Invalid host, username or password. Please Retry.';
-		}
-	}
-} 
-else 
+        if (is_object($jsonResult)) {
+            if ($jsonResult->error != '') {
+                $message = $jsonResult->error;
+            } else {
+                $message = "Your account does not have an API key. Please, generate one in your Mailrelay's account: Settings -> API access -> Generate new API key.";
+            }
+        } else {
+            $message = 'Invalid host. Please Retry.';
+        }
+    } else {
+        update_option('jsonResult', $jsonResult);
+        add_action('admin_menu', 'web_admin_actions2');
+    }
+}
+else
 {
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
+    $userhost = get_option('userhost');
+    $mailrelay_api_key = get_option('mailrelay_api_key');
 }
 
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step3') )
+if(isset($_REQUEST['step']) && ($_REQUEST['step'] == 'step3'))
 {
-	$querystr = "SELECT * FROM $wpdb->users";
-	$users = $wpdb->get_results($querystr, OBJECT);
-	$groups=$_POST['group'];
+    $querystr = "SELECT * FROM $wpdb->users";
+    $users = $wpdb->get_results($querystr, OBJECT);
+    $groups = $_POST['group'];
 
-	//Form data sent
-	$usrname = $_POST['usrname'];
-	update_option('usrname', $usrname);
-	
-	$pwd = $_POST['pwd'];
-	update_option('pwd', $pwd);
-	
-	$userhost = $_POST['userhost'];
-	update_option('userhost', $userhost);
-	update_option('step', 'step2');
+    //Form data sent
+    $userhost = $_POST['userhost'];
+    $mailrelay_api_key = $_POST['mailrelay_api_key'];
+    update_option('userhost', $userhost);
+    update_option('mailrelay_api_key', $mailrelay_api_key);
+    update_option('step', 'step2');
 
-	// These will be entered by user.
-	$username = get_option('usrname');
-	$password = get_option('pwd');
-	$hostname = get_option('userhost');
+    // These will be entered by user.
+    $hostname = get_option('userhost');
+    $mailrelay_api_key = get_option('mailrelay_api_key');
 
-	// First thing, authenticate
-	$url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
-	$curl = curl_init($url);
+    // First thing, authenticate
+    $url = 'http://'. $hostname .'/ccm/admin/api/version/2/&type=json';
+    $curl = curl_init($url);
 
-	$params = array(
-		'function' => 'doAuthentication',
-		'username' => $username,
-		'password' => $password
-	);
+    $added = 0;
+    $updated = 0;
+    $fail = 0;
 
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_POST, 1);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+    foreach($users as $user)
+    {
+        $user->user_email;
+        // Call getSubscribers
+        $params = array(
+            'function' => 'getSubscribers',
+            'apiKey' => $mailrelay_api_key,
+            'email' => $user->user_email,
+        );
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
         $headers = array(
-                'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
+            'X-Request-Origin: Wordpress|1.2.0|'.$wp_version
         );
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-	// Call the page, it will return a json
-	$result = curl_exec($curl);
-	$jsonResult = json_decode($result);
+        $result = curl_exec($curl);
+        $jsonResult = json_decode($result);
 
-	if(trim($jsonResult->status)==1)
-	{
-		$added=0;
-		$updated=0;
-		$fail=0;
+        if(count($jsonResult->data) > 0)
+        {
+            $params = array(
+                'function' => 'updateSubscriber',
+                'apiKey' => $mailrelay_api_key,
+                'id' => $jsonResult->data[0]->id,
+                'email' => $user->user_email,
+                'name' => $user->display_name,
+                'groups' => $groups
+            );
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
-		if (!$jsonResult->status) 
-		{
-			throw new Exception('Authenticate failed. Please verify hostname, username and password.');
-		}
-		else
-		{
-			$apiKey = $jsonResult->data;
-		}
+            $headers = array(
+                'X-Request-Origin: Wordpress|1.2.0|'.$wp_version
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-		foreach($users as $user)
-		{
-			$user->user_email;
-			// Call getSubscribers
-			$params = array(
-				'function' => 'getSubscribers',
-				'apiKey' => $apiKey,
-				'email'=>$user->user_email,
-			);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+            $result = curl_exec($curl);
+            $jsonResult = json_decode($result);
 
-        		$headers = array(
-                		'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        		);
-        		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            if($jsonResult->status == 1)
+            {
+                $updated++;
+            }
+            else
+            {
+                $fail++;
+            }
+        } else {
+            $params = array(
+                'function' => 'addSubscriber',
+                'apiKey' => $mailrelay_api_key,
+                'email' => $user->user_email,
+                'name' => $user->display_name,
+                'groups' => $groups
+            );
 
-			$result = curl_exec($curl);
-			$jsonResult = json_decode($result);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
 
-			if(count($jsonResult->data)>0)
-			{
-				$params = array(
-					'function' => 'updateSubscriber',
-					'apiKey' => $apiKey,
-					'id'=>$jsonResult->data[0]->id,
-					'email'=>$user->user_email,
-					'name'=>$user->display_name,
-					'groups'=>$groups
-				);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+            $headers = array(
+                'X-Request-Origin: Wordpress|1.2.0|'.$wp_version
+            );
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
-        			$headers = array(
-                			'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        			);
-        			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            $result = curl_exec($curl);
+            $jsonResult = json_decode($result);
 
-				$result = curl_exec($curl);
-				$jsonResult = json_decode($result);
+            if($jsonResult->status == 1)
+            {
+                $added++;
+            }
+            else
+            {
+                $fail++;
+            }
+        }
+    }
+    update_option('added', $added);
+    update_option('updated', $updated);
+    update_option('fail', $fail);
 
-				if($jsonResult->status==1)
-				{
-					$updated++;
-				}
-				else
-				{
-					$fail++;
-				}
-			}
-			else
-			{
-				$params = array(
-					'function' => 'addSubscriber',
-					'apiKey' => $apiKey,
-					'email'=>$user->user_email,
-					'name'=>$user->display_name,
-					'groups'=>$groups
-				);
-
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
-
-        			$headers = array(
-                			'X-Request-Origin: Wordpress|1.1.1|'.$wp_version 
-        			);
-        			curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-				$result = curl_exec($curl);
-				$jsonResult = json_decode($result);
-
-				if($jsonResult->status==1)
-				{
-					$added++;
-				}
-				else
-				{
-					$fail++;
-				}
-			}
-		}
-		update_option('added', $added);
-		update_option('updated', $updated);
-		update_option('fail', $fail);
-	}
-	add_action('admin_menu', 'web_admin_actions3');
-} 
-else 
+    add_action('admin_menu', 'web_admin_actions3');
+}
+else
 {
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
+    $userhost = get_option('userhost');
+    $mailrelay_api_key = get_option('mailrelay_api_key');
 }
 ?>
--- src/step2.php
+++ src/step2.php
@@ -1,67 +1,64 @@
-<?php 
-if(isset($_REQUEST['step']) && ($_REQUEST['step']=='step2') )
+<?php
+if (isset($_REQUEST['step']) && ($_REQUEST['step'] == 'step2'))
 {
-	$jsonResult = get_option('jsonResult');
-	$data=$jsonResult->data;
-	$usrname = get_option('usrname');
-	$pwd = get_option('pwd');
-	$userhost = get_option('userhost');
-	
+    $jsonResult = get_option('jsonResult');
+    $data = $jsonResult->data;
+    $userhost = get_option('userhost');
+    $mailrelay_api_key = get_option('mailrelay_api_key');
 ?>
 
 <div class="wrap">
 <?php
-        screen_icon('options-general'); 
+        screen_icon('options-general');
         echo "<h2 id='mailrelay_settings'>";
-        echo _e( 'Mairelay Step 2 - Choose groups', 'mailrelay' ) . "</h2>"; 
+        echo _e('Mairelay Step 2 - Choose groups', 'mailrelay') . "</h2>";
 ?>
 
-<form name="webservices_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
-	<input type="hidden" name="chk_hidden" value="Y" />
-	<input type="hidden" name="step" value="step3" />
-	<input type="hidden" name="usrname" value="<?php echo $usrname; ?>">
-	<input type="hidden" name="pwd" value="<?php echo $pwd; ?>" />
-	<input type="hidden" name="userhost" value="<?php echo $userhost; ?>">
+<form name="webservices_form" method="post" action="<?php echo str_replace('%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+    <input type="hidden" name="chk_hidden" value="Y" />
+    <input type="hidden" name="step" value="step3" />
+    <input type="hidden" name="userhost" value="<?php echo $userhost; ?>">
+    <input type="hidden" name="mailrelay_api_key" value="<?php echo $mailrelay_api_key; ?>">
 
-	<table class="form-table">
-		<tr><th scope="row">
-		<label for="group"><?php _e( 'Please Select Group', 'mailrelay' ); ?></label></th>
-		<td>
-		<select multiple="multiple" name="group[]" size="5" style="height:auto;">
-			<?php foreach($data as $x=>$value){ ?>
-			<option value="<?php echo $data[$x]->id; ?>"><?php echo $data[$x]->name; ?></option>
-			<?php } ?>
-		</select>
-		<p><?php _e("All your Wordpress users will be synced with the groups you are choosing now.", "mailrelay"); ?><br />
-		<?php _e("To create new groups in Mailrelay, you must login into the control panel and click into the Mail Relay > Subscribers groups", "mailrelay"); ?><br />
-		<?php _e("Once there you can add a new group for your Wordpress users, or edit an existing one", "mailrelay"); ?></p>
-		</td></tr>
-	</table>
+    <table class="form-table">
+        <tr><th scope="row">
+        <label for="group"><?php _e('Please Select Group', 'mailrelay'); ?></label></th>
+        <td>
+        <select multiple="multiple" name="group[]" size="5" style="height:auto;">
+            <?php foreach($data as $x=>$value){ ?>
+            <option value="<?php echo $data[$x]->id; ?>"><?php echo $data[$x]->name; ?></option>
+            <?php } ?>
+        </select>
+        <p><?php _e("All your Wordpress users will be synced with the groups you are choosing now.", "mailrelay"); ?><br />
+        <?php _e("To create new groups in Mailrelay, you must login into the control panel and click into the Mail Relay > Subscribers groups", "mailrelay"); ?><br />
+        <?php _e("Once there you can add a new group for your Wordpress users, or edit an existing one", "mailrelay"); ?></p>
+        </td></tr>
+    </table>
 
-	<p class="submit">
-	<input type="button" onclick="return chk_form();" name="Select groups" value="<?php _e('Select groups', 'mailrelay' ) ?>" class="button-primary" />
-	</p>
+    <p class="submit">
+    <input type="button" onclick="return chk_form();" name="Select groups" value="<?php _e('Select groups', 'mailrelay') ?>" class="button-primary" />
+    </p>
 </form>
 
 </div>
 <script type="text/javascript">
 function chk_form()
 {
-	var chk=check();
-	if(chk!=false)
-	{
-		document.webservices_form.submit();
-		document.webservices_form.action="";
-	}
+    var chk=check();
+    if (chk != false)
+    {
+        document.webservices_form.submit();
+        document.webservices_form.action = "";
+    }
 }
-     function check() {
-		if(document.webservices_form["group[]"].value == "")
-		{
-			alert("<?php echo _e('Please select at least one Group.', 'mailrelay'); ?>");
-			return false; 
-		}
-		return true;
-	}
-  </script>
+function check() {
+    if(document.webservices_form["group[]"].value == "")
+    {
+        alert("<?php echo _e('Please select at least one Group.', 'mailrelay'); ?>");
+        return false;
+    }
+    return true;
+}
+ </script>
 
 <?php } ?>
