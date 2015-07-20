0ae3f4e7a62039d2ae14833de7ac67400bf8295d
Fernando Morgenstern <contato@fernandomarcelo.com>
2015-07-20 17:17:26 -0300

    Prepare release of version 1.7.2

--- src/mailrelay.php
+++ src/mailrelay.php
@@ -3,7 +3,7 @@
 Plugin Name: Mailrelay
 Plugin URI: http://mailrelay.com
 Description: Easily sync your Wordpress users with Mailrelay.
-Version: 1.7.1
+Version: 1.7.2
 Author: Mailrelay.com
 */
 
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 }
 
 if (!defined('MAILRELAY_PLUGIN_VERSION')) {
-    define('MAILRELAY_PLUGIN_VERSION', '1.7.1');
+    define('MAILRELAY_PLUGIN_VERSION', '1.7.2');
 }
 
 if (function_exists('is_admin') && is_admin()) {
