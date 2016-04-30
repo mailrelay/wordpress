aa651fbbcbc41045f955c54f73ec75fd4447c867
Fernando Morgenstern <contato@fernandomarcelo.com>
2016-04-30 14:58:45 -0300

    Add option to automatically sync new users

--- src/connection_settings.php
+++ src/connection_settings.php
@@ -9,8 +9,14 @@ if (!empty($message)) {
     echo _e($message, 'mailrelay');
 }
 
+wp_enqueue_script('mailrelay_main', '/wp-content/plugins/mailrelay/assets/js/main.js');
+
 $mailrelay_host = get_option('mailrelay_host');
 $mailrelay_api_key = get_option('mailrelay_api_key');
+$mailrelay_auto_sync = get_option('mailrelay_auto_sync');
+$mailrelay_auto_sync_groups = get_option('mailrelay_auto_sync_groups');
+
+$groups = mailrelay_get_groups();
 
 ?>
 
@@ -34,6 +40,28 @@ echo _e('Connection Settings', 'mailrelay') . '</h2>';
         <?php _e('Please enter your API key. You can generate your API key on your Mailrelay panel, Configuration -> API access -> Generate API key',  'mailrelay'); ?>
         </p>
         </td>
+        </tr>
+
+        <tr>
+            <th scope="row">
+                <label for="mailrelay_auto_sync"><?php _e('Auto sync new users', 'mailrelay'); ?></label>
+            </th>
+            <td>
+                <input type="checkbox" name="mailrelay_auto_sync" id="mailrelay_auto_sync" <?php echo $mailrelay_auto_sync ? 'checked' : '' ?> />
+            </td>
+        </tr>
+
+        <tr id="mailrelay_auto_sync_groups_wrapper">
+            <th scope="row">
+                <label for="mailrelay_auto_sync_groups"><?php _e('Auto sync groups', 'mailrelay'); ?></label>
+            </th>
+            <td>
+                <select multiple="multiple" name="mailrelay_auto_sync_groups[]" id="mailrelay_auto_sync_groups" size="5" style="height:auto;">
+                    <?php foreach($groups->data as $value) { ?>
+                    <option value="<?php echo $value->id; ?>" <?php echo in_array($value->id, $mailrelay_auto_sync_groups) ? 'selected' : '' ?>><?php echo esc_html($value->name); ?></option>
+                    <?php } ?>
+                </select>
+            </td>
         </tr>
     </table>
 <?php
