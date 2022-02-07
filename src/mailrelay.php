6c90717ed4eaa22ea61b61d0f1f81397bbf099fc
Fernando Morgenstern <contato@fernandomarcelo.com>
2022-02-07 09:18:52 -0300

    Release version 2.0.1

--- src/mailrelay.php
+++ src/mailrelay.php
@@ -3,7 +3,7 @@
  * Plugin Name: Mailrelay
  * Plugin URI: https://mailrelay.com
  * Description: Synchronize your WordPress users with Mailrelay
- * Version: 2.0
+ * Version: 2.0.1
  * Author: Mailrelay
  * Text Domain: Mailrelay
  * Domain Path: /languages
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Access Denied.' );
 }
 
-define( 'MAILRELAY_PLUGIN_VERSION', '2.0' );
+define( 'MAILRELAY_PLUGIN_VERSION', '2.0.1' );
 
 require_once __DIR__ . '/inc/common.php';
 require_once __DIR__ . '/inc/global-hooks.php';
--- src/readme.txt
+++ src/readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: mailrelay,newsletter,email marketing
 Requires at least: 5.0
 Tested up to: 5.9
-Stable tag: 2.0
+Stable tag: 2.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
