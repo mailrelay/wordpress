aa2bc227df8a9843860bb6c4cb071e70bddccdc1
brunonzanini <98592514+brunonzanini@users.noreply.github.com>
2022-02-10 11:09:44 -0300

    Add wp_register_script() to load the js on footer

--- src/inc/class-mailrelaypages.php
+++ src/inc/class-mailrelaypages.php
@@ -30,7 +30,8 @@ class MailrelayPages {
 		}
 
 		wp_enqueue_style( 'mailrelay-admin-css', ( plugins_url( 'mailrelay/css/style.css' ) ), array(), MAILRELAY_PLUGIN_VERSION );
-		wp_enqueue_script( 'mailrelay-admin-js', (plugins_url( 'mailrelay/js/main.js') ), array(), MAILRELAY_PLUGIN_VERSION, true ) ;
+		wp_register_script( 'mailrelay-admin-js', (plugins_url( 'mailrelay/js/main.js') ), array(), MAILRELAY_PLUGIN_VERSION, true );
+		wp_enqueue_script( 'mailrelay-admin-js' );
 	}
 
 	public function render_admin_page() {
