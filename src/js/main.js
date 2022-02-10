caaa6e54fcef87aa00c897da17132844f2540329
brunonzanini <98592514+brunonzanini@users.noreply.github.com>
2022-02-10 10:11:48 -0300

    Adds functions to test settings form
    Hide Groups if auto sync is off
    Alert user that he needs to select a group if auto sync is on

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+jQuery( document ).ready(function() {
+    if (jQuery('#mailrelay_auto_sync').is(':checked') == false ) {
+        jQuery('#mailrelay_groups').hide();
+    }
+    jQuery('#mailrelay_auto_sync').on('change', function() {
+        jQuery('#mailrelay_groups').toggle();
+         if (jQuery('#mailrelay_auto_sync').is(':checked')) {
+            jQuery("#manual-form").submit(function(){
+                if (jQuery.trim(jQuery('#mailrelay_auto_sync_groups').val()).length == 0){
+                    alert('Please select at least on group');
+                    return false;
+                }
+            });
+        } else {
+            return true;
+        }
+    });
+});
+
+
