be63b51b3f3027fdf874b1c2b09ddb2b319940e6
Jose Arenas Romero <arenas.jose@pucp.pe>
2022-04-21 09:35:47 -0500

    Added form block preview

--- src/inc/block-widgets.php
+++ src/inc/block-widgets.php
@@ -1,8 +1,8 @@
 <?php // phpcs:ignore Squiz.Commenting.FileComment.Missing
 
 function mailrelay_wpforms_init() {
-
-	wp_register_script( 'mailrelay-wpforms-block', plugin_dir_url( __FILE__ ) . '../js/mailrelay-wpforms-block.js', array( 'wp-blocks', 'wp-i18n' ), true, false );
+	$js_file = '../js/mailrelay-wpforms-block.js';
+	wp_register_script( 'mailrelay-wpforms-block', plugin_dir_url( __FILE__ ) . $js_file, array( 'wp-blocks', 'wp-i18n' ), filemtime( plugin_dir_path( __FILE__ ) . $js_file ), false );
 
 	$all_forms['forms'] = mailrelay_get_signup_forms();
 
@@ -10,45 +10,8 @@ function mailrelay_wpforms_init() {
 	register_block_type(
 		'mailrelay/mailrelay-wpforms',
 		array(
-			'editor_script'   => 'mailrelay-wpforms-block',
-			'render_callback' => 'mailrelay_wpforms_callback',
+			'editor_script' => 'mailrelay-wpforms-block',
 		)
 	);
 }
 add_action( 'init', 'mailrelay_wpforms_init' );
-
-function mailrelay_wpforms_callback( $attributes, $content, $block_instance ) {
-
-	$form_id = ! empty( $attributes['form_id'] ) ? $attributes['form_id'] : '';
-	ob_start();
-	?>
-	<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-		<?php
-		if ( isset( $form_id ) && (int) $form_id > 0 ) {
-			$response = mailrelay_get_signup_forms( $form_id );
-			if ( ! empty( $response ) && count( $response ) > 0 ) {
-				echo $response[0]['embedded_form_code']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-		}
-		?>
-	</div>
-	<?php
-	return ob_get_clean();
-}
-
-function mailrelay_api_init() {
-
-	register_rest_route(
-		'mailrelay-wpforms',
-		'/get-signup-forms',
-		array(
-			'methods'             => 'GET',
-			'callback'            => 'mailrelay_get_signup_forms',
-			'permission_callback' => function () {
-				return true;
-			},
-		)
-	);
-}
-
-add_action( 'rest_api_init', 'mailrelay_api_init' );
--- src/js/mailrelay-wpforms-block.js
+++ src/js/mailrelay-wpforms-block.js
@@ -1,35 +1,84 @@
-wp.blocks.registerBlockType('mailrelay/mailrelay-wpforms', {
+wp.blocks.registerBlockType('mailrelay/mailrelay-wpforms', { // phpcs:ignore Squiz.Commenting.FileComment.Missing
+	apiVersion: 2,
 	title: 'Mailrelay forms',
 	category: 'widgets',
 	icon: 'email',
 	description: 'Select and display one of your forms.',
 	attributes: {
 		form_id: {
-			type: 'string',
-			default: null
+			type: 'integer',
+			default: 0
 		}
 	},
 	keywords: ['mailrelay'],
-	edit: (props) => {
-		var form_id = props.attributes.form_id;
-		var all_forms = mailrelay_wpforms_forms.forms;
-		var all_options = [{label: 'Select a Form', value: 0}];
-		for(var i=0;i<all_forms.length;i++) {
+	edit: function (props) {
+		let blockProps = wp.blockEditor.useBlockProps();
+		let form_id = props.attributes.form_id;
+		let all_forms = mailrelay_wpforms_forms.forms;
+		let all_options = [{label: 'Select a Form', value: 0}];
+		
+		for(let i=0;i<all_forms.length;i++) {
 			all_options.push({label: all_forms[i].name, value: all_forms[i].id});
 		}
 		
-		return wp.element.createElement('div', { className: 'wpforms-gutenberg-form-selector-wrap'},
+		let display = wp.element.createElement('div', { className: 'wpforms-gutenberg-form-selector-wrap'},
 			wp.element.createElement(wp.components.SelectControl,
 				{
 					value: form_id,
 					options: all_options,
 					onChange: function( value ) {
-						props.setAttributes({ form_id: value });
+						props.setAttributes({ form_id: Number(value) });
 					}
 				}
 			)
 		);
+
+		if (all_forms.length > 0) {
+			let script_src = all_forms[0].embedded_form_code.match(/<script.*?src="(.*?)"[^>]*>/)[1];
+			if (script_src && !document.getElementById('mailrelay-form-script')) {
+				let script_el = document.createElement('script');
+				script_el.type = "text/javascript";
+				script_el.id = 'mailrelay-form-script';
+				script_el.src = script_src;
+
+				document.head.appendChild(script_el);
+			}
+		}
+		
+		let selected_form = mailrelay_wpforms_forms.forms.find(function(v) { return v.id === props.attributes.form_id });
+		if(selected_form) {
+			display = [wp.element.createElement( 'div', blockProps, wp.element.RawHTML( { children: selected_form.embedded_form_code } ) )];
+			display.push([wp.element.createElement(
+				wp.blockEditor.InspectorControls,
+				null,
+				wp.element.createElement(
+					wp.components.PanelBody,
+					null,
+					wp.element.createElement( wp.components.SelectControl, 
+						{
+							value: form_id,
+							label: 'Select a Form',
+							options: all_options,
+							onChange: function( value ) {
+								props.setAttributes({ form_id: Number(value) });
+							}
+						} 
+					)
+				)
+			)]);
+		}
+		
+		return display;
 	
 	},
-	save: () => { return null }
-});
+	save: function( props ) {
+		let blockProps = wp.blockEditor.useBlockProps.save();
+
+		let selected_form = mailrelay_wpforms_forms.forms.find(function(v) { return v.id === props.attributes.form_id });
+		if (!selected_form) {
+			return;
+		}
+
+		return wp.element.createElement( 'div', blockProps, wp.element.RawHTML( { children: selected_form.embedded_form_code } ) );
+	}
+})
